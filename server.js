eb2a368f5a531c2e57f69017e30add4a1f363269
jhonatan <jhonatancarbonell912@gmail.com>
2026-09-12 00:25:34 -0500

    Primer commit - proyecto base con Express

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Servir archivos estáticos desde la carpeta actual
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
